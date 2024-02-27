c0a174786c5fdf0983204c6d392377575661f974
alfatta <arwinnersyah@gmail.com>
2024-02-27 10:22:51 +0700

    add google verification id

--- config/app.ts
+++ config/app.ts
@@ -4,4 +4,6 @@ export default {
     "Highly motivated software developer with >6 years of experience in designing and implementing software solutions. Dedicated to building high-quality software that exceeds expectations.",
   appKeyword:
     "software developer, software development, web development, app development",
+  googleSiteId: "CYI7UG73r6aeqQle8QgnHmdBGVgaVyCLHvY_vvsd9vU",
+  umamiSiteId: "180c0c1d-ef9f-4955-955a-3af09b2bcbcd",
 };
